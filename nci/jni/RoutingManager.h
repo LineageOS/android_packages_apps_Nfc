52164b6ef33a2308d423c87a9eef1d212cc2a53d
Simon Shields <simon@lineageos.org>
2017-02-16 09:45:31 +1100

    NfcNci: make T3T/Nfc-F HCE optional
    Some devices (bacon, klte) don't work with T3T HCE, and attempting to
    enable it will instead completely break HCE. Allow these devices to work
    around the issue by disabling NFC-F HCE.
    
    Add ENABLE_NFCF_HCE=0 to libnfc-nci.conf to disable. Default is enabled.
    
    Change-Id: I0ccabe10cc83a01ae6614d69d2bba33731a84fdd
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- nci/jni/RoutingManager.h
+++ nci/jni/RoutingManager.h
@@ -27,6 +27,8 @@
 #include "nfa_api.h"
 #include "nfa_ee_api.h"
 
+#define NAME_ENABLE_NFCF_HCE "ENABLE_NFCF_HCE"
+
 using namespace std;
 
 class RoutingManager {
@@ -103,6 +105,7 @@ class RoutingManager {
   int mAidMatchingMode;
   int mNfcFOnDhHandle;
   bool mIsScbrSupported;
+  bool mNfcFEnabled;
   uint16_t mDefaultSysCode;
   uint16_t mDefaultSysCodeRoute;
   uint8_t mDefaultSysCodePowerstate;
