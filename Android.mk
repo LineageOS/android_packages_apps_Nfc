ce1a510342abeff7600ef4e6c0eb829455796bc6
Adrian DC <radian.dc@gmail.com>
2017-04-10 01:24:43 +0200

    NFC: Restore legacy NXP stack
    Author: Adrian DC <radian.dc@gmail.com>
    Date:   Mon Apr 10 01:24:43 2017 +0200
    
        Revert "Merge "NFC: Remove obsolete nxp code.""
    
        This reverts commit 4a1fae62e709d64612884fcda8e001fefb2d4e26, reversing
        changes made to aa96d0701b980dbc795cdde6a7e8232eb2f3d0d4.
    
        Change-Id: I167c606b1b8654f556e6891917da5bc67e30c6db
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Oct 15 18:03:45 2015 +0100
    
        Revert "Stop building NXP stack."
    
        This reverts commit 988c3fff5470a1de3a880bd07fa438cc47e283c8.
    
        Change-Id: I787ea2b8a2c6977cefb1ad72952b8a1ed7a52660
    
    Author: Adrian DC <radian.dc@gmail.com>
    Date:   Mon Apr 10 01:24:43 2017 +0200
    
        Nfc: NXP: Forward port for AOSP P
    
        "packages/apps/Nfc: Set LOCAL_SDK_VERSION where possible."
    
        Change-Id: I8a7bd3f592340d553ac94a9d3285fe2952485a24
    
    Change-Id: I240fc333cbcd9c727e0dcfec28523b7124eaf85f

--- Android.mk
+++ Android.mk
@@ -1,6 +1,30 @@
 LOCAL_PATH:= $(call my-dir)
 
 ########################################
+# NXP Configuration
+########################################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+        $(call all-java-files-under, src)
+
+LOCAL_SRC_FILES += \
+        $(call all-java-files-under, nxp)
+
+LOCAL_PACKAGE_NAME := Nfc
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_CERTIFICATE := platform
+
+LOCAL_JNI_SHARED_LIBRARIES  := libnfc_jni
+
+LOCAL_PROGUARD_ENABLED := disabled
+
+include $(BUILD_PACKAGE)
+
+########################################
 # NCI Configuration
 ########################################
 include $(CLEAR_VARS)
@@ -14,6 +38,8 @@ LOCAL_SRC_FILES += \
         $(call all-java-files-under, nci)
 
 LOCAL_PACKAGE_NAME := NfcNci
+LOCAL_OVERRIDES_PACKAGES := Nfc
+
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 
