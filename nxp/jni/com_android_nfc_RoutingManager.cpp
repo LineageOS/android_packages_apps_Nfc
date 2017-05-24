d84f618ef2ce0c2dd67e752fd13660b977afecb1
Chippa-a <vusal1372@gmail.com>
2017-05-24 22:43:25 +0300

    Nfc: Add minimal RoutingManager for NXP
    Unfortunately Google abandoned support for NXP,
    that's why we get some troubles like this.
    
    This patch provide a minimal RoutingManager based on NCI code
    to resolve FC due to not implemented functions like:
    * doGetDefaultRouteDestination
    * doGetDefaultOffHostRouteDestination
    * doGetAidMatchingMode
    introduced in ea8f37ee5376e296c3b67f57946b73e48ad88c5c and
    31208d3ee36f583fd998c89508a3e93bb550cb29
    
    With this now possible to use HCE on NXP based chip (pn544 for example).
    
    Change-Id: I2d0964835e66a8651fbd93d16aa56649bace9f55

---
+++ nxp/jni/com_android_nfc_RoutingManager.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ *  Manage the listen-mode routing table.
+ */
+
+#include <cutils/log.h>
+#include <ScopedLocalRef.h>
+#include <JNIHelp.h>
+#include "RoutingManager.h"
+
+const JNINativeMethod RoutingManager::sMethods [] =
+{
+    {"doGetDefaultRouteDestination", "()I", (void*) RoutingManager::com_android_nfc_cardemulation_doGetDefaultRouteDestination},
+    {"doGetDefaultOffHostRouteDestination", "()I", (void*) RoutingManager::com_android_nfc_cardemulation_doGetDefaultOffHostRouteDestination},
+    {"doGetAidMatchingMode", "()I", (void*) RoutingManager::com_android_nfc_cardemulation_doGetAidMatchingMode}
+};
+
+RoutingManager::RoutingManager ()
+{
+    static const char fn [] = "RoutingManager::RoutingManager()";
+
+    // Set the "default" route
+    mDefaultEe = 0x00;
+    ALOGD("%s: default route is 0x%02X", fn, mDefaultEe);
+
+    // Set the "default" route for Nfc-F
+    mDefaultEeNfcF = 0x00;
+    ALOGD("%s: default route for Nfc-F is 0x%02X", fn, mDefaultEeNfcF);
+
+    // Set the default "off-host" route.  This is hard-coded at the Java layer
+    // but we can override it here to avoid forcing Java changes.
+    mOffHostEe = 0xf4;
+
+    mAidMatchingMode = AID_MATCHING_EXACT_ONLY;
+
+    ALOGD("%s: mOffHostEe=0x%02X", fn, mOffHostEe);
+}
+
+RoutingManager::~RoutingManager ()
+{
+
+}
+
+RoutingManager& RoutingManager::getInstance ()
+{
+    static RoutingManager manager;
+    return manager;
+}
+
+int RoutingManager::registerJniFunctions (JNIEnv* e)
+{
+    static const char fn [] = "RoutingManager::registerJniFunctions";
+    ALOGD ("%s", fn);
+    return jniRegisterNativeMethods (e, "com/android/nfc/cardemulation/AidRoutingManager", sMethods, NELEM(sMethods));
+}
+
+int RoutingManager::com_android_nfc_cardemulation_doGetDefaultRouteDestination (JNIEnv*)
+{
+    return getInstance().mDefaultEe;
+}
+
+int RoutingManager::com_android_nfc_cardemulation_doGetDefaultOffHostRouteDestination (JNIEnv*)
+{
+    return getInstance().mOffHostEe;
+}
+
+int RoutingManager::com_android_nfc_cardemulation_doGetAidMatchingMode (JNIEnv*)
+{
+    return getInstance().mAidMatchingMode;
+}
