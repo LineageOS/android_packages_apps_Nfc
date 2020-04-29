cd23ac948bcc1f5ce504c018c5878265232b37a4
Bruno Martins <bgcngm@gmail.com>
2020-04-29 23:40:03 +0100

    nxp: Add support for ISO DEP extended length
    This is basically inspired on the NCI implementation merged upstream in commit 5d5dd8a.
    The difference is that, instead of reading the config from an NFC configuration file,
    it checks whether the feature is supported based on a system property.
    The reason is that NXP's old ass stack doesn't allow reading configs from files.
    
    Change-Id: I30083cdd55de42b7f452e4137e18f66d5f5360d4

--- nxp/src/com/android/nfc/dhimpl/NativeNfcManager.java
+++ nxp/src/com/android/nfc/dhimpl/NativeNfcManager.java
@@ -57,6 +57,7 @@ public class NativeNfcManager implements DeviceHost {
     /* Native structure */
     private long mNative;
 
+    private int mIsoDepMaxTransceiveLength;
     private final DeviceHostListener mListener;
     private final Context mContext;
 
@@ -120,9 +121,13 @@ public class NativeNfcManager implements DeviceHost {
 
     private native boolean doInitialize();
 
+    private native int getIsoDepMaxTransceiveLength();
+
     @Override
     public boolean initialize() {
-        return doInitialize();
+        boolean ret = doInitialize();
+        mIsoDepMaxTransceiveLength = getIsoDepMaxTransceiveLength();
+        return ret;
     }
 
     private native void doEnableDtaMode();
@@ -346,12 +351,7 @@ public class NativeNfcManager implements DeviceHost {
             case (TagTechnology.NFC_V):
                 return 253; // PN544 RF buffer = 255 bytes, subtract two for CRC
             case (TagTechnology.ISO_DEP):
-                /* The maximum length of a normal IsoDep frame consists of:
-                 * CLA, INS, P1, P2, LC, LE + 255 payload bytes = 261 bytes
-                 * such a frame is supported. Extended length frames however
-                 * are not supported.
-                 */
-                return 261; // Will be automatically split in two frames on the RF layer
+                return mIsoDepMaxTransceiveLength;
             case (TagTechnology.NFC_F):
                 return 252; // PN544 RF buffer = 255 bytes, subtract one for SoD, two for CRC
             default:
@@ -392,8 +392,8 @@ public class NativeNfcManager implements DeviceHost {
 
     @Override
     public boolean getExtendedLengthApdusSupported() {
-        // Not supported on the PN544
-        return false;
+        /* 261 is the default size if extended length frames aren't supported */
+        return mIsoDepMaxTransceiveLength > 261;
     }
 
     @Override
