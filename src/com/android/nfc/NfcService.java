c6bbc4d593d092224b6383572d432c8d99030c18
Jack Yu <jackcwyu@google.com>
2020-11-03 23:33:40 +0800

    Avoid crashing Nfc NfcService for Nfc hardware recovery
    Do not crash NfcService directly when trying to recover Nfc hardware.
    
    Bug: 153704838
    Test: nfc service will disable and enable to reinitialize NFCC when nci
    timeout detected.
    
    Change-Id: Icaef380d0382eb0dbae0cdf81b8dddb6555465cf

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -421,6 +421,12 @@ public class NfcService implements DeviceHostListener {
         new ApplyRoutingTask().execute();
     }
 
+    @Override
+    public void onHwErrorReported() {
+        new EnableDisableTask().execute(TASK_DISABLE);
+        new EnableDisableTask().execute(TASK_ENABLE);
+    }
+
     final class ReaderModeParams {
         public int flags;
         public IAppCallback callback;
