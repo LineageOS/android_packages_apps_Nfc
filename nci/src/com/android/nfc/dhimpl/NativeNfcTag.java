63b9196918c0e43ad9c57c6467965c05f4d87169
LuK1337 <priv.luk@gmail.com>
2021-03-27 23:08:34 +0100

    NativeNfcTag: Treat MIFARE_CLASSIC same as NFC_A in getTechExtras()
    NxpNfc bringup introduced a change to NfcA class that
    appears to change what extras get parsed in case of
    MIFARE_CLASSIC tags.
    
    While cherry-picking the whole change would be annoying
    due to 500+ lines diff in JNI, adding MIFARE_CLASSIC
    to switch block should bring the behavior back to pre-fwb
    patch state.
    
    Change-Id: Ib26115a63996e17a661b1df63e4878bfa0ac48b6

--- nci/src/com/android/nfc/dhimpl/NativeNfcTag.java
+++ nci/src/com/android/nfc/dhimpl/NativeNfcTag.java
@@ -663,7 +663,8 @@ public class NativeNfcTag implements TagEndpoint {
             for (int i = 0; i < mTechList.length; i++) {
                 Bundle extras = new Bundle();
                 switch (mTechList[i]) {
-                    case TagTechnology.NFC_A: {
+                    case TagTechnology.NFC_A:
+                    case TagTechnology.MIFARE_CLASSIC: {
                         byte[] actBytes = mTechActBytes[i];
                         if ((actBytes != null) && (actBytes.length > 0)) {
                             extras.putShort(NfcA.EXTRA_SAK, (short) (actBytes[0] & (short) 0xFF));
