f84e6e7f9d0a854c6cf9e4d488b143fe0d05a74f
chloedai <chloedai@google.com>
2021-10-04 10:52:26 +0000

    Add routing table dumping function to dumpsys nfc
    >> nci/jni/NativeNfcManager.cpp
    Add functions to Java native interface to provide methods for routing
    config retrieving and the max routing table size
    
    >> nci/src/com/android/nfc/dhimpl/NativeNfcManager.java
    Override the definition in DeviceHost.java
    
    >> src/com/android/nfc/DeviceHost.java
    Add functions to get the routing configuration and the max routing
    table size
    
    >> src/com/android/nfc/NfcService.java
    Call functions to dump the routing table
    
    >> src/com/android/nfc/RoutingTableParser.java
    Provides methods to dump the routing information
    
    Bug: 201984397
    Test: O6/B5 test pass
    Merged-In: If164d8f027242a2b7068c31618b8892b109dca0e
    Change-Id: If164d8f027242a2b7068c31618b8892b109dca0e

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -322,6 +322,7 @@ public class NfcService implements DeviceHostListener {
     TagService mNfcTagService;
     NfcAdapterService mNfcAdapter;
     NfcDtaService mNfcDtaService;
+    RoutingTableParser mRoutingTableParser;
     boolean mIsDebugBuild;
     boolean mIsHceCapable;
     boolean mIsHceFCapable;
@@ -469,6 +470,7 @@ public class NfcService implements DeviceHostListener {
 
         mNfcTagService = new TagService();
         mNfcAdapter = new NfcAdapterService();
+        mRoutingTableParser = new RoutingTableParser();
         Log.i(TAG, "Starting NFC service");
 
         sService = this;
@@ -3420,6 +3422,9 @@ public class NfcService implements DeviceHostListener {
                 mCardEmulationManager.dump(fd, pw, args);
             }
             mNfcDispatcher.dump(fd, pw, args);
+            if (mState == NfcAdapter.STATE_ON) {
+                mRoutingTableParser.dump(mDeviceHost, pw);
+            }
             copyNativeCrashLogsIfAny(pw);
             pw.flush();
             mDeviceHost.dump(fd);
