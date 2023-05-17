04bb49dbf123a8fa28fe7acba3956b94d4a5bfd9
Roshan Pius <rpius@google.com>
2023-05-17 10:27:49 -0700

    nfc(service): Add device config P/H flag monitoring class
    Added NFC antenna blocked alert as a first P/H config flag that can be
    updated via server side flags.
    
    Bug: 282814176
    Test: manual compilation
    Change-Id: Id6eb08cbe94cb3abe1afd4aa07560926d542d3e2

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -353,6 +353,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
     // for recording the latest Tag object cookie
     long mCookieUpToDate = -1;
 
+    private DeviceConfigFacade mDeviceConfigFacade;
     private NfcDispatcher mNfcDispatcher;
     private PowerManager mPowerManager;
     private KeyguardManager mKeyguard;
@@ -547,6 +548,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
         } else {
             mInProvisionMode = false;
         }
+        mDeviceConfigFacade = new DeviceConfigFacade(mContext, mHandler);
 
         mNfcDispatcher = new NfcDispatcher(mContext, mHandoverDataParser, mInProvisionMode);
 
@@ -636,7 +638,7 @@ public class NfcService implements DeviceHostListener, ForegroundUtils.Callback 
 
         // Notification message variables
         mDispatchFailedCount = 0;
-        if (mContext.getResources().getBoolean(R.bool.enable_antenna_blocked_alert) &&
+        if (mDeviceConfigFacade.isAntennaBlockedAlertEnabled() &&
             !mPrefs.getBoolean(PREF_ANTENNA_BLOCKED_MESSAGE_SHOWN, ANTENNA_BLOCKED_MESSAGE_SHOWN_DEFAULT)) {
             mAntennaBlockedMessageShown = false;
             mDispatchFailedMax =
