99aa183a8df3f1f21262d2aedbb12b1628e315ee
Alisher Alikhodjaev <alisher@google.com>
2023-10-31 11:13:03 -0700

    Possible deadlock on the NfcService object
    Bug: 268038643
    Bug: 307489565
    Test: CtsVerifier
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2d8ea70c65831313d73784fb3c78f64ff0cbd627)
    Merged-In: I683ae425dafa4e209b9517b62ada7d8a694f84a9
    Change-Id: I683ae425dafa4e209b9517b62ada7d8a694f84a9

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -852,9 +852,7 @@ public class NfcService implements DeviceHostListener {
     }
 
     public boolean isSecureNfcEnabled() {
-        synchronized (NfcService.this) {
-            return mIsSecureNfcEnabled;
-        }
+        return mIsSecureNfcEnabled;
     }
 
     final class NfcAdapterService extends INfcAdapter.Stub {
