3f2ecaac01b63b9a298880c97c34df31a042ca06
Alisher Alikhodjaev <alisher@google.com>
2025-02-28 15:25:55 -0800

    Several BAL bypasses in HostApduService.
    Implement unbinding the service on onBindingDied or onNullBinding
    
    This will make sure that we are not hanging around waiting for a
    service that has crashed.
    
    Bug: 385917501
    Test: atest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:012edb08545c676c57595a3fb93c6566d797d459)
    Merged-In: Ie08ec7e57c6eaf04960e659922592957d77097e6
    Change-Id: Ie08ec7e57c6eaf04960e659922592957d77097e6

--- src/com/android/nfc/cardemulation/HostEmulationManager.java
+++ src/com/android/nfc/cardemulation/HostEmulationManager.java
@@ -499,6 +499,19 @@ public class HostEmulationManager {
                 mPaymentServiceUserId = -1;
             }
         }
+
+        @Override
+        public void onBindingDied(ComponentName name) {
+            synchronized (mLock) {
+                unbindPaymentServiceLocked();
+            }
+        }
+        @Override
+        public void onNullBinding(ComponentName name) {
+            synchronized (mLock) {
+                unbindPaymentServiceLocked();
+            }
+        }
     };
 
     private ServiceConnection mConnection = new ServiceConnection() {
@@ -530,6 +543,20 @@ public class HostEmulationManager {
                 mServiceName = null;
                 mServiceBound = false;
                 mServiceUserId = -1;
+            }
+        }
+
+        @Override
+        public void onBindingDied(ComponentName name) {
+            synchronized (mLock) {
+                unbindServiceIfNeededLocked();
+            }
+        }
+
+        @Override
+        public void onNullBinding(ComponentName name) {
+            synchronized (mLock) {
+                unbindServiceIfNeededLocked();
             }
         }
     };
