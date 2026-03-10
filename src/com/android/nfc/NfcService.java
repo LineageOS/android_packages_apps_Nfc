e846d5296bc8aecef5fbc34787becd5673837a12
Kyle Hsiao <kylehsiao@google.com>
2026-03-10 01:03:09 -0800

    Load the Secure NFC setting when the current user is changed upon query
    Bug: 489303684
    Bug: 368376056
    Test: Manual
    Flag: EXEMPT BUGFIX
    (cherry picked from commit 6babe35bf1d9c8b5343a008fc0f4f9674eda8c14)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:0a87553b4ed56a384ed4dd308c2c86d7aead146d
    Merged-In: I8a6668138499f9930c68c7d420dbff8c5e4ede64
    Change-Id: I8a6668138499f9930c68c7d420dbff8c5e4ede64

--- src/com/android/nfc/NfcService.java
+++ src/com/android/nfc/NfcService.java
@@ -1048,6 +1048,10 @@ public class NfcService implements DeviceHostListener {
         @Override
         public boolean isNfcSecureEnabled() throws RemoteException {
             synchronized (NfcService.this) {
+                int current_userId = ActivityManager.getCurrentUser();
+                if (mUserId != current_userId) {
+                    loadSecureNfcSettings(current_userId);
+                }
                 return mIsSecureNfcEnabled;
             }
         }
